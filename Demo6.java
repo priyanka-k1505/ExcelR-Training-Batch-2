20d234db843abc7a3f22c4ea1ecf244800caf60e
priyanka-k1505 <2311cs010294@mallareddyuniversity.ac.in>
2025-01-03 10:16:22 +0530

    username and password login

---
+++ Demo6.java
@@ -0,0 +1,10 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        String username = "priyanka";
+        String pwd = "priyanka@123";
+        while((username == "priyanka") && (pwd == "priyanka@123")) {
+            System.out.println("hello priyanka");
+            break;
+        }
+    }
+}
