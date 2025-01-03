a63148c9a988bec97c76035dfda8161f6ec2d5f4
priyanka-k1505 <2311cs010294@mallareddyuniversity.ac.in>
2025-01-03 09:53:42 +0530

    verifying credentials

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        string username = "user001";
+        string pwd = "user001";
+        while((username == "user001") && (pwd == "user 001")) {
+            System.out.println(x:'hello user happy new year');
+            break;
+        }
+    }
+}
