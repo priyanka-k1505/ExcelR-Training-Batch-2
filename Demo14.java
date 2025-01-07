22c5eadb5e3ab6875b602821c4d56e25c507f7f9
priyanka-k1505 <2311cs010294@mallareddyuniversity.ac.in>
2025-01-07 10:14:31 +0530

    Unary operators

---
+++ Demo14.java
@@ -0,0 +1,14 @@
+public class Demo14   
+{  
+public static void main(String args[])  
+{  
+int i = 10;  
+i++;  
+System.out.println(i);  
+++i;                 
+System.out.println(i);  
+System.out.println(++i);  
+System.out.println(i++);  
+System.out.println(i);  
+}  
+}  
