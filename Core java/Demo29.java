e98fcb6c33433b07edb64fb9d15f8734c88d4ad3
priyanka-k1505 <2311cs010294@mallareddyuniversity.ac.in>
2025-01-20 09:42:00 +0530

    array with 10 friends names

---
+++ Core java/Demo29.java
@@ -0,0 +1,9 @@
+public class Demo29 {
+    public static void main(String[] args) {
+        String[] friends = {"Priyanka", "jhansi", "anu", "kavita", "tharuni", "pandu", "rohith", "roshan", "rishi", "Madhan"};
+        System.out.println("List of Friends:");
+        for (String friend : friends) {
+            System.out.println(friend);
+        }
+    }
+}
