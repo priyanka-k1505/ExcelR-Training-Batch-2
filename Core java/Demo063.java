c32997cc1f91be71f2aab77ec432404168ac3d38
priyanka-k1505 <2311cs010294@mallareddyuniversity.ac.in>
2025-02-05 09:58:06 +0530

    sum of first and last element in an array

---
+++ Core java/Demo063.java
@@ -0,0 +1,7 @@
+public class Demo063{
+    public static void main(String[] args) {
+        int[] arr = {12,14,16,18};
+        int sum = arr[0] + arr[arr.length-1];
+        System.out.println("The sum of first and last elements is: "+sum);
+    }
+}
